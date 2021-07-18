6226da9d03c3713a342ffb62eb374b8898f1b68d
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-18 10:23:42 -0500

    add and delete a friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -69,6 +69,31 @@ const userController = {
           res.json(dbUserData);
         })
         .catch(err => res.status(400).json(err));
+    },
+
+    //post to add a new friend to a user's friend list
+    
+    addFriend({ params }, res){
+        User.findOneAndUpdate({
+            id_: params.id
+        },
+        {$push: { friends: params.friendsId}},
+        {new: true},
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    //delet to remove a friend from a user's friend list
+    deleteFriend({ params }, res) {
+        User.findOneAndDelete({
+            id_: params.id
+        },
+        {$pull: { friends: params.friendsId }},
+        {new: true},
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
     }
   };
 
